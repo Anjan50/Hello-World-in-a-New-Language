854f64d46e2871d4de0b4a3b49f3a60d3624c1e9
Anjan Diyora <49095535+Anjan50@users.noreply.github.com>
2021-01-11 20:34:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("What's up Techies");
+            
+        }
+    }
+}
